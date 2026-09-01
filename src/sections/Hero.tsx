b779730de582cefd8bb41093ec8b3557e4db57b7
arkin-bp <arkin@bpoptima.com>
2026-09-01 17:07:09 +0530

    Remove Talk to Sales button from Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -327,9 +327,6 @@ export default function Hero() {
               See the Platform
               <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="group-hover:translate-x-1 transition-transform duration-300"><path d="M5 12h14M12 5l7 7-7 7" /></svg>
             </MagneticButton>
-            <MagneticButton href="#cta" variant="ghost">
-              Talk to Sales
-            </MagneticButton>
           </div>
         </div>
 
